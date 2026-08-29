47da094d6b93febc53114d5d46b7b008ff062944
2400032450Deepak <2400032450@kluniversity.in>
2026-08-29 12:22:45 +0530

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      margin: 0,
+      backgroundColor: '#ffffff'
+    }}>
+      <h1 style={{
+        fontSize: '4.5rem',
+        fontWeight: 'bold',
+        color: '#000000',
+        fontFamily: 'sans-serif',
+        textAlign: 'center'
+      }}>
+        Welcome to my React app
+      </h1>
+    </div>
+  );
+}
+
+export default App;
