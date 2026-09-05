65b1319f71a777488f4d3bf7beca87c395a5f0a7
2400032450Deepak <2400032450@kluniversity.in>
2026-09-05 08:49:48 +0530

    completed registration & login

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,8 @@
-import './App.css';
+import NavBar from './pages/NavBar.jsx'
+import './style.css'
 
 function App() {
-  return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-      margin: 0,
-      backgroundColor: '#ffffff'
-    }}>
-      <h1 style={{
-        fontSize: '4.5rem',
-        fontWeight: 'bold',
-        color: '#000000',
-        fontFamily: 'sans-serif',
-        textAlign: 'center'
-      }}>
-        Welcome to my React app
-      </h1>
-    </div>
-  );
+  return <NavBar />
 }
 
-export default App;
+export default App
